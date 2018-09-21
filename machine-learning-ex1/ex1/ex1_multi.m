2720627d1b8e57c6f346f044345991d58877a4fe
Dor Segal <dor@recongate.com>
2018-09-21 18:13:06 +0300

    finished ex1_multi and implemented normalEqn

--- machine-learning-ex1/ex1/ex1_multi.m
+++ machine-learning-ex1/ex1/ex1_multi.m
@@ -82,13 +82,12 @@ X = [ones(m, 1) X];
 fprintf('Running gradient descent ...\n');
 
 % Choose some alpha value
-alpha = 0.01;
-num_iters = 400;
+num_iters = 50;
+alph = 1;
 
 % Init Theta and Run Gradient Descent 
 theta = zeros(3, 1);
-[theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters);
-
+[theta, J_history] = gradientDescentMulti(X, y, theta, alph, num_iters);
 % Plot the convergence graph
 figure;
 plot(1:numel(J_history), J_history, '-b', 'LineWidth', 2);
@@ -104,8 +103,7 @@ fprintf('\n');
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
-price = 0; % You should change this
-
+price = (([1 1650 3] - [0 mu]) ./ [1 sigma]) * theta;
 
 % ============================================================
 
@@ -149,7 +147,7 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
+price = [1 1650 3] * theta;
 
 
 % ============================================================
